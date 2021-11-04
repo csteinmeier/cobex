80a4f735e8a45ddcf999f1394340a61362ec7137
Christine Steinmeier <csteinmeier@fh-bielefeld.de>
2021-11-04 16:56:15 +0100

    added new UI for audio recordings

--- app/src/main/java/com/example/tinetest1/CaptureSound.kt
+++ app/src/main/java/com/example/tinetest1/CaptureSound.kt
@@ -1,10 +1,12 @@
 package com.example.tinetest1
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.tinetest1.databinding.FragmentCaptureSoundBinding
 
 /**
@@ -17,6 +19,8 @@ class CaptureSound : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var environmentSounds = false;
+    private var started = false;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +31,61 @@ class CaptureSound : Fragment() {
         return binding.root
 
     }
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-/*        binding.buttonBackCreate.setOnClickListener {
-            findNavController().navigate(R.id.action_CreateNew_to_SecondFragment)
-        }*/
+        toggleSoundInputType()
+
+        binding.buttonEnvironment.setOnClickListener {
+            environmentSounds = true
+            toggleSoundInputType()
+        }
+        binding.buttonMusic.setOnClickListener {
+            environmentSounds = false
+            toggleSoundInputType()
+        }
+        binding.buttonStart.setOnClickListener {
+            toggleRecording()
+        }
+
+        var web = binding.webView
+        web.setBackgroundColor(Color.TRANSPARENT); //for gif without background
+        web.loadUrl("file:///android_asset/htmls/gif.html");
+        web.visibility= View.INVISIBLE
+    }
+
+    private fun toggleRecording() {
+        if (!started)
+        {
+            started = true
+            binding.buttonStart.text = "Stop"
+            binding.webView.visibility= View.VISIBLE
+            startRecording()
+        }
+        else
+        {
+            started = false
+            binding.buttonStart.text = "Start"
+            binding.webView.visibility= View.INVISIBLE
+        }
+    }
+
+    private fun startRecording() {
+        TODO("Not yet implemented")
+    }
+
+    private fun toggleSoundInputType() {
+        if (environmentSounds)
+        {
+            binding.buttonEnvironment.setBackgroundColor(Color.BLACK)
+            binding.buttonMusic.setBackgroundColor(Color.parseColor("#FF6200EE"))
+        }
+        else
+        {
+            binding.buttonMusic.setBackgroundColor(Color.BLACK)
+            binding.buttonEnvironment.setBackgroundColor(Color.parseColor("#FF6200EE"))
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/tinetest1/FirstFragment.kt
+++ app/src/main/java/com/example/tinetest1/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.example.tinetest1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tinetest1.databinding.FragmentFirstBinding
 
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.buttonStart.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
